a9070dbfb6e56ffc85ca73c9f6bdfd6584d68d70
Scapular Steam <86719656+ScapularSteam@users.noreply.github.com>
2025-02-24 15:45:00 +0000

    fixed airtable loader 🥳

--- portfolio/src/content/config.ts
+++ portfolio/src/content/config.ts
@@ -1,6 +1,7 @@
 // This script creates a content collection of articles from records fetched from the Airtable API
 import { defineCollection } from "astro:content";
 import { airtableLoader } from "@ascorbic/airtable-loader";
+import base from "airtable";
 
 // Create content collection
 const articles = defineCollection({
@@ -9,10 +10,11 @@ const articles = defineCollection({
   loader: airtableLoader({
 
     // Remember to hide in environment variables before open-sourcing
-    base: "x8mCb4g7BRaEgj",
-    table: "AfOGmIkNU7GU6B",
+    base: "appx8mCb4g7BRaEgj",
+    table: "tblAfOGmIkNU7GU6B",
     token: "patMsnAV1tR0ixNdV.6d666b812606dbebbdf95536a8226019d82aca59d46d3b3bc6333b11e4ad7cfd",
   }),
 });
 
+console.log('Articles fetched from base', base)
 export const collections = { articles };
